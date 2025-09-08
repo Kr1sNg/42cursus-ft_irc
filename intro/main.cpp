3eac046048dd215d46c7b79bb95279b44c4dded6
Kr1s Ng <piq.k0olz@gmail.com>
2025-09-08 22:18:44 +0200

    intro

--- intro/main.cpp
+++ intro/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tat-nguy <tat-nguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 22:57:44 by tat-nguy          #+#    #+#             */
-/*   Updated: 2025/09/06 23:42:08 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2025/09/08 10:40:59 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,17 +38,33 @@ void	Server::CloseFds(void)
 void	Server::SerSocket(void)
 {
 	struct sockaddr_in	add; // std struct contains info about server address (IPv4)
-	struct pollfd		NewPoll;
+	struct pollfd		NewPoll; // used for monitoring fds for I/O events.
+	// its commonly employed with poll() system call to perform multiplexed I/O
+	// allowing progr to wait for events on multiple fds simultanouesly without having to resort to blocking I/O operations 
 	
 	add.sin_family = AF_INET; //-> set the address family to IPv4
 	add.sin_port = htons(this->_Port); //->convert 16bit unsigned short int from host byte order to network byte order
-	add.sin_addr.s_addr = INADDR_ANY; //-> set add to any local machine addr
+	add.sin_addr.s_addr = INADDR_ANY; //-> represent any IP add, meaning socket will be bound to all available network interfaces on the host.
 	
 	_SerSocketFd = socket(AF_INET, SOCK_STREAM, 0); //-> create server socket
 	if (_SerSocketFd == -1)
 		throw std::runtime_error("fail to create socket");
 	
 	int en = 1;
+	// set the socket option (SO_REUSEADD) to reuse the address
+	if (setsockopt(_SerSocketFd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1)
+		throw std::runtime_error("failed to set option (SO_REUSEADDR) on socket");
+	// set the socket option (O_Nonblock) for non-blocking socket
+	if (fcntl(_SerSocketFd, F_SETFL, O_NONBLOCK) == -1)
+		throw std::runtime_error("failed to set option (O_NONBLOCK) on socket");
+	// bind the socket to address
+	if (bind(_SerSocketFd, (struct sockaddr *)&add, sizeof(add)) == -1)
+		throw std::runtime_error("failed to bind socket");
+	// listen for incomming connections and making the socket a passive socket
+	if (listen(_SerSocketFd, SOMAXCONN) == -1)
+		throw std::runtime_error("listen() failed");
+	
+	
 	
 }
 
